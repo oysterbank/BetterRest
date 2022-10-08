5328bbd0556ff987f5166c5335658c8aab2fcf6f
Kris Laratta <krislaratta@gmail.com>
2022-10-08 13:19:35 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kris Laratta on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
